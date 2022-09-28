2dcac847dce253f5124c5ca8e05eb75e2e91b2b1
daniels <daniel.shchepin@gmail.com>
2022-09-28 21:16:43 +0700

    bugfix img-task

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,6 +1,6 @@
 export const img = () => {
     return $.gulp.src($.path.img.src)
-        .pipe($.plugin.newer($.path.img.dest))
+        .pipe($.plugin.newer($.path.img.src))
         .pipe($.plugin.if($.isProd, $.plugin.webp()))
         .pipe($.plugin.if($.isProd, $.gulp.dest(`${$.path.img.dest}/webp`, !`${$.path.img.dest}`)))
         .pipe($.plugin.if($.isProd, $.plugin.imagemin($.options.imagemin)))
