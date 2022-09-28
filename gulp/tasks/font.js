d09a868eff52c000b8332f300ddd597d2b26e3d7
daniels <daniel.shchepin@gmail.com>
2022-09-28 21:36:43 +0700

    bugfix font-task

--- gulp/tasks/font.js
+++ gulp/tasks/font.js
@@ -1,8 +1,8 @@
 export const font = () => {
     return $.gulp.src($.path.font.src)
-            .pipe($.plugin.newer($.path.font.dest))
-            .pipe($.plugin.fonter($.options.fonter))
-            .pipe($.gulp.dest($.path.font.dest))
-            .pipe($.plugin.ttf2woff2())
+            .pipe($.plugin.newer($.path.font.src))
+            .pipe($.plugin.if($.isProd, $.plugin.fonter($.options.fonter)))
+            .pipe($.plugin.if($.isProd, $.gulp.dest($.path.font.dest)))
+            .pipe($.plugin.if($.isProd, $.plugin.ttf2woff2()))
             .pipe($.gulp.dest($.path.font.dest))
 }
