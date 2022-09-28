10e3ac2ddc9d4a454737ca65abfe3f2e4f221a35
daniels <daniel.shchepin@gmail.com>
2022-09-28 22:17:42 +0700

    bugfix gulp

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -10,6 +10,12 @@ export const path = {
         dest: pathDest
     },
 
+    css: {
+        src: pathSrc + '/css/*.css',
+        watch: pathSrc + '/css/**/*.css',
+        dest: pathDest + '/css'
+    },
+
     scss: {
         src: pathSrc + '/scss/**/*.{scss, sass}',
         watch: pathSrc + '/scss/**/*.{scss, sass}',
